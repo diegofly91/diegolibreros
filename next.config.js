71d01b17e895181e226dc029e891ce4e6959c41a
diegofly91 <diego_fernandolibreros@hotmail.com>
2021-03-24 21:23:49 -0300

    resolviendo problema diploy de la web

--- next.config.js
+++ next.config.js
@@ -8,5 +8,6 @@ module.exports = withPlugins([[withSass], [withImages]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
-  }
+  },
+  distDir: "_next"
 });
