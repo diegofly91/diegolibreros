275343882d7b7e2eb318cd343253d729d5db6485
diegofly91 <diego_fernandolibreros@hotmail.com>
2021-03-25 02:11:02 -0300

    Updates

--- _next/static/PekS1-HYSgHrgP_7Y1pVc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fbee240a3.cf6f3d0f925dfa37d1b9.js","static\u002Fchunks\u002Fpages\u002Findex-a05284c4bf8cc240830c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9b1f8a545b7e39cd7f61.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
