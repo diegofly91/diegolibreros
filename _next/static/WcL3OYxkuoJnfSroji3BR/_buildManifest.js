ce3f4e23e7605117546b4a5255bd1ce53c166f1a
diegofly91 <diego_fernandolibreros@hotmail.com>
2023-03-07 14:07:20 -0500

    Updates

---
+++ _next/static/WcL3OYxkuoJnfSroji3BR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/672-72d5553e7f60d7a1.js","static/chunks/pages/index-d535bb917c3f8319.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
